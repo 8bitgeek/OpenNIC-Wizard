65fd17f2262c405a503449a569c61c979adb36ec
Mike Sharkey <mike@8bitgeek.net>
2012-02-25 20:32:26 +0000

    beginning of client/server split

---
+++ server/main.cpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2012 Mike Sharkey <michael_sharkey@firstclass.com>
+ *
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * Mike Sharkey wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.
+ */
+#include <QtGui/QApplication>
+#include "opennic.h"
+
+#include <QSystemTrayIcon>
+#include <QMessageBox>
+#include <QDateTime>
+
+int main(int argc, char *argv[])
+{
+	int rc=0;
+	qsrand(QDateTime::currentDateTime().toTime_t()); /* seed random numbers */
+	Q_INIT_RESOURCE(opennic);
+	QApplication a(argc, argv);
+	a.setQuitOnLastWindowClosed(false);
+	if ( !QSystemTrayIcon::isSystemTrayAvailable() )
+	{
+		QMessageBox::critical(0, QObject::tr("OpenNic"), QObject::tr("No System Tray Detected."));
+		rc = 1;
+	}
+	else
+	{
+		OpenNIC on;
+		QObject::connect(&on,SIGNAL(quit()),&a,SLOT(quit()));
+		rc = a.exec();
+	}
+	Q_CLEANUP_RESOURCE(opennic);
+	return rc;
+}
+
---
+++ server/opennictest.h
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2012 Mike Sharkey <michael_sharkey@firstclass.com>
+ *
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * Mike Sharkey wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.
+ */
+#ifndef OPENNICTESTER_H
+#define OPENNICTESTER_H
+
+#include <QThread>
+#include "opennicdns.h"
+#include <QList>
+#include <QMutex>
+#include <QDateTime>
+#include <QHostAddress>
+#include <QTimerEvent>
+
+#define PURGE_TIMEOUT	120		/* purge timeout in seconds */
+
+class OpenNICTest : public QObject
+{
+	Q_OBJECT
+	public:
+
+		typedef struct
+		{
+			QHostAddress	addr;		/* The host ip address */
+			QString			name;		/* The host name to query */
+			QString			mxName;		/* The mx host name to query */
+			QDateTime		start;		/* The start of the query */
+			qint64			latency;	/* Latency in mulliseconds */
+			int				error;		/* Error Code */
+		} query;
+
+		explicit OpenNICTest(QObject *parent=0);
+		virtual ~OpenNICTest();
+
+		OpenNICDns*			dns() {return mDns;}
+
+	public slots:
+
+		void				resolve(QHostAddress addr, QString name, quint16 port=DEFAULT_DNS_PORT);
+
+	signals:
+
+		void				queryResult(OpenNICTest::query* result);
+
+	protected slots:
+
+		void				reply(dns_cb_data& rdata);
+
+	protected:
+
+		virtual void		timerEvent(QTimerEvent* e);
+
+	private:
+		void				dispose(query* q);
+		void				append(query* q);
+		query*				find(void* context);
+		void				purge();
+		int					mSecondTimer;
+		QList<query*>		mQueries;
+		OpenNICDns*			mDns;
+};
+
+#endif // OPENNICTESTER_H
