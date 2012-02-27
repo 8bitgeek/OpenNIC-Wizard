6c6502bf7509b17c68a647c5f6bd7e2a3cb1c05c
eightbitgeek <eightbitgeek@5fb8db59-1db5-4d4e-a380-d223f689dc3d>
2012-02-27 02:10:07 +0000

    New resolver pool and resolver pool item class for containing and evaluating resolvers.

---
+++ branches/beta1/server/opennicresolverpool.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2012 Mike Sharkey <michael_sharkey@firstclass.com>
+ *
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * Mike Sharkey wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.
+ */
+#ifndef OPENNICRESOLVERPOOL_H
+#define OPENNICRESOLVERPOOL_H
+
+#include <QObject>
+#include <QList>
+#include "opennicresolverpoolitem.h"
+
+class OpenNICResolverPool : public QObject
+{
+		Q_OBJECT
+	public:
+		OpenNICResolverPool(QObject *parent = 0);
+		OpenNICResolverPool(const OpenNICResolverPool& other);
+		virtual ~OpenNICResolverPool();
+
+		OpenNICResolverPool&			copy(OpenNICResolverPool& other);
+		OpenNICResolverPool				fastest(int num);
+		bool							contains(OpenNICResolverPoolItem item);
+		bool							contains(QHostAddress item);
+		QList<OpenNICResolverPoolItem>&	items()			{return mItems;}
+		int								count()			{mItems.count();}
+		OpenNICResolverPoolItem			at(int pos)		{return items().at(pos);}
+
+	public slots:
+		void							sort();
+
+	private:
+		void							swap(int a,int b);
+		QList<OpenNICResolverPoolItem>	mItems;
+};
+
+
+#endif // OPENNICRESOLVERPOOL_H
---
+++ branches/beta1/server/opennicresolverpoolitem.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2012 Mike Sharkey <michael_sharkey@firstclass.com>
+ *
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * Mike Sharkey wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.
+ */
+#ifndef OPENNICRESOLVERPOOLITEM_H
+#define OPENNICRESOLVERPOOLITEM_H
+
+#include <QObject>
+#include <QHostAddress>
+#include <QDateTime>
+#include <QString>
+
+#define RESOLVER_SAMPLE_STORAGE_LIMIT		10			/* number of samples to remember for averaging */
+
+class OpenNICResolverPoolItem : public QObject
+{
+	Q_OBJECT
+	public:
+
+		OpenNICResolverPoolItem(QObject *parent = 0);
+		OpenNICResolverPoolItem(const OpenNICResolverPoolItem& other);
+		virtual ~OpenNICResolverPoolItem();
+
+		OpenNICResolverPoolItem&	copy(const OpenNICResolverPoolItem& other);
+		QHostAddress				hostAddress()		{return mHostAddress;}
+		int							testCount()			{return mTestCount;}
+		int							replyCount()		{return mReplyCount;}
+		int							failCount()			{return testCount()-replyCount();}
+		int							lastLatency()		{return mLatencySamples.count() ? mLatencySamples.at(mLatencySamples.count()-1) : 0;}
+		double						averageLatency();
+		QDateTime					lastReply()			{return mLastReply;}
+		QDateTime					lastTimeout()		{return mLastTimeout;}
+		QString						lastFault()			{return mLastFault;}
+		bool						alive()				{return lastReply() > lastTimeout();}
+
+		OpenNICResolverPoolItem&	operator=(const OpenNICResolverPoolItem& other);
+		bool						operator==(OpenNICResolverPoolItem &other);
+		bool						operator>(OpenNICResolverPoolItem &other);
+		bool						operator<(OpenNICResolverPoolItem &other);
+		bool						operator>=(OpenNICResolverPoolItem &other);
+		bool						operator<=(OpenNICResolverPoolItem &other);
+
+	public slots:
+		void						clear();
+
+	private:
+		QHostAddress				mHostAddress;		/* host address wrapper */
+		int							mTestCount;			/* number of tests conducted */
+		int							mReplyCount;		/* number of replies received */
+		QList<int>					mLatencySamples;	/* last latency samples (msec) */
+		QDateTime					mLastReply;			/* the time of teh last reply */
+		QDateTime					mLastTimeout;		/* the time of the last timeout */
+		QString						mLastFault;			/* the last fault message */
+};
+
+
+#endif // OPENNICRESOLVERPOOLITEM_H
