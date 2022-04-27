dbe8e755778f75c3f28de3afd313b5da66901597
8bitgeek <mike@8bitgeek.net>
2022-04-27 00:25:16 -0400

    some install stuff

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+DEST_BIN=/usr/local/bin
+DEST_ETC=/usr/local/etc
+
+cp server/OpenNICServer $DEST_BIN/
+cp client/OpenNIC $DEST_BIN/
+
+cp server/bootstrap.domains $DEST_ETC/
+cp server/bootstrap.t1 $DEST_ETC/
+
