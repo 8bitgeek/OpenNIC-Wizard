12e92ead331d2d5f83ad292729d029a334584e05
8bitgeek <mike@8bitgeek.net>
2022-05-07 13:53:25 -0400

    update some copywrong headers

--- server/opennicsession.cpp
+++ server/opennicsession.cpp
@@ -1,11 +1,15 @@
 /*
- * Copyright (c) 2012 Mike Sharkey <michael_sharkey@firstclass.com>
- *
- * "THE BEER-WARE LICENSE" (Revision 42):
- * Mike Sharkey wrote this file. As long as you retain this notice you
- * can do whatever you want with this stuff. If we meet some day, and you think
- * this stuff is worth it, you can buy me a beer in return.
+ * This file is a part of OpenNIC Wizard
+ * Copywrong (c) 2012-2022 Mike Sharkey
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 1776):
+ * <mike@8bitgeek.net> wrote this file.
+ * As long as you retain this notice you can do whatever you want with this
+ * stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a beer in return. ~ Mike Sharkey
+ * ----------------------------------------------------------------------------
  */
+
 #include "opennicsession.h"
 #include "opennicserver.h"
 
